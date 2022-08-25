7a075c8b522901da2ad3357a9de7f2454a842f8b
Blake Harrington <baharrington25@students.bmchs.org>
2022-08-25 14:37:08 -0500

    Unit1_Project.py

---
+++ Unit1_Project.py
@@ -0,0 +1,40 @@
+'''
+# Story
+Your Typical Day: 
+I had just gotten out of bed, about ot get ready for school when a massive _____ flew past my window!
+I looked outside and saw twenty people _____ when it passed them! I assumed it was an alien ____ because it was so fast!
+ I ran to my mom as _____ as I could and told her I thought it was going to _____. 
+She said I was crazy and that my _____ must have been thinking for me and that brain was out of order.
+It made me so _____ that I _____ at her and ran out the door. But outside my door was something even crazier!
+I saw a ______  _____ and it _____ at me! It started to grab me so I hit it with my _____ and knocked it out.
+Right as I stepped outside the ground disappeared and I started falling! But, as soon as I hit the floor I woke up instantly.
+It was a dream? I thought it was just a friday.
+'''
+
+# Introduction
+print("Hi, welcome to Your Typical Day: A Mad Libs Story.\n"
+"I am going to ask you for parts of speech and with those I will create a fun story!")
+
+# Input Variables
+noun_input = input("Enter a noun: ")
+adverb_input = input("Enter an adverb: ")
+body_part_input = input("Enter a body part: ")
+adjective_input = input("Enter an adjective: ")
+proper_noun_input = input("Enter a proper noun: ")
+adjective_input_2 = input("Enter another adjective: ")
+body_part_input_2 = input("Enter another body part: ")
+verb_input_3 = input("Enter another verb ending with -ed: ")
+verb_input = input("Enter an action verb ending with -ing: ")
+verb_input_2 = input("Enter an action verb ending with -ed: ")
+animal_or_monster_input = input("Enter an animal or monster: ")
+
+
+# Completed Story
+print(f"Your Typical Day: \n" f"I had just gotten out of bed, about ot get ready for school when a massive {noun_input} flew past my window!" "\n"
+f"I looked outside and saw twenty people {verb_input} when it passed them! I assumed it was an alien {noun_input} because it was so fast!\n"
+f"I ran to my mom as {adverb_input} as I could and told her I thought it was going after {proper_noun_input}.\n"
+f"She said I was crazy and that my {body_part_input} must have been thinking for me and that brain was out of order.\n"
+f"It made me so {adjective_input} that I {verb_input_2} her and ran out the door. But outside my door was something even crazier!\n"
+f"I saw a {adjective_input_2} {animal_or_monster_input} and it {verb_input_3} at me! It started to grab me so I hit it with my {body_part_input_2} and knocked it out.\n"
+"Right as I stepped outside the ground disappeared and I started falling! But, as soon as I hit the floor I woke up instantly." "\n"
+"It was a dream? I thought it was just a friday.")
